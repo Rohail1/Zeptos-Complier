ec0573cdef0466f9b6371ee57c2b610ad06f47dd
RoXas <rohail_najam@hotmail.com>
2014-09-08 17:37:03 +0500

    Add FileIO

---
+++ Zaptos/Zaptos/fileIO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Zaptos
+{
+    class fileIO
+    {
+        public void write(string path,string[] lines)
+        {
+           File.WriteAllLines(path, lines);
+        }
+    }
+}
